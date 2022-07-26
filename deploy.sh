d7d9547b4492f7e24c5870ebe25a34e157fe5750
misscod10 <misscod10@gmail.com>
2022-07-26 08:55:58 +0200

    Con tomcat

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #/bin/bash
+
 yum update -y
-yum install -y java-1.8.0-openjdk-devel wget unzip
-cp geoserver.service /usr/lib/systemd/system/geoserver.service
-cd /tmp
-wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.2/geoserver-2.19.2-bin.zip
-mkdir /usr/share/geoserver
-unzip -d /usr/share/geoserver/ geoserver-2.19.2-bin.zip
-useradd -m -U -d /usr/share/geoserver -s /bin/false geoserver
-chown -R geoserver:geoserver /usr/share/geoserver
-systemctl daemon-reload
-systemctl enable --now geoserver.service
+
+yum install -y java-1.8.0-openjdk-devel wget unzip tomcat
+
+wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.21.0/geoserver-2.21.0-war.zip
+
+unzip -d /var/lib/tomcat/webapps/ geoserver-2.21.0-war.zip
+
+systemctl restart tomcat
