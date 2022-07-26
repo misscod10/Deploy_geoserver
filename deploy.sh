a9d2876e711dabec3c6edd35170820cd1550ebb9
misscod10 <misscod10@gmail.com>
2022-07-26 09:12:30 +0200

    BURN ME TO TH EGROUND

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-yum update -y
-
 yum install -y java-1.8.0-openjdk-devel wget unzip tomcat
 
 wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.21.0/geoserver-2.21.0-war.zip
