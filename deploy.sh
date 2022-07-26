5e1b9658646add5dca66502c9becfdfa0b367de8
misscod10 <misscod10@gmail.com>
2022-07-26 09:08:43 +0200

    Burn me to the ground

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,11 @@ wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.21.0/geoserver
 unzip -d /var/lib/tomcat/webapps/ geoserver-2.21.0-war.zip
 
 systemctl restart tomcat
+
+cd $CATALINA_HOME (where Tomcat binaries are installed)
+mkdir -p ./lib/org/apache/catalina/util/
+cat > ./lib/org/apache/catalina/util/ServerInfo.properties <<EOF
+server.info=
+server.number=
+server.built=
+EOF
