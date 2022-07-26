df01db86c3a0dc9840398e6320a851b587b4bb5b
misscod10 <misscod10@gmail.com>
2022-07-26 10:04:57 +0200

    constructed alternatebly

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,12 @@ unzip -d /var/lib/tomcat/webapps/ geoserver-2.21.0-war.zip
 
 systemctl restart tomcat
 
-cd $CATALINA_HOME (where Tomcat binaries are installed)
+cd $CATALINA_HOME 
 mkdir -p ./lib/org/apache/catalina/util/
 cat > ./lib/org/apache/catalina/util/ServerInfo.properties <<EOF
 server.info=
 server.number=
 server.built=
 EOF
+
+sed -i 127i\        <Valve className="org.apache.catalina.valves.ErrorReportValve" showReport="false" showServerInfo="false" /> /usr/share/tomcat/conf/server.xml
