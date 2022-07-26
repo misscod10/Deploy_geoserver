259a232c992237f70c6b0e17c5cc306098fb25c2
misscod10 <misscod10@gmail.com>
2022-07-26 12:09:29 +0200

    pain

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@ yum install -y java-1.8.0-openjdk-devel wget unzip tomcat
 
 wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.21.0/geoserver-2.21.0-war.zip --no-check-certificate
 unzip -d /var/lib/tomcat/webapps/ geoserver-2.21.0-war.zip
-
-systemctl restart tomcat
-
 cd $CATALINA_HOME 
 mkdir -p ./lib/org/apache/catalina/util/
 cat > ./lib/org/apache/catalina/util/ServerInfo.properties <<EOF
@@ -13,5 +10,5 @@ server.info=
 server.number=
 server.built=
 EOF
-
 sed -i 127i'<Valve className="org.apache.catalina.valves.ErrorReportValve" showReport="false" showServerInfo="false" />' /usr/share/tomcat/conf/server.xml
+systemctl restart tomcat
